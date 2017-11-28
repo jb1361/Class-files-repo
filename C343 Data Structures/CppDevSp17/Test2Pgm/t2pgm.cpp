02f354bd9066ea5e22c2d5426558e4bb4ec72759
jb1361 <justinbutler4@hotmail.com>
2017-11-28 01:54:15 -0500

    updated test2

--- C343 Data Structures/CppDevSp17/Test2Pgm/t2pgm.cpp
+++ C343 Data Structures/CppDevSp17/Test2Pgm/t2pgm.cpp
@@ -9,11 +9,8 @@ void reverse(SequenceOfIntegers& s) {
 	s.remove(0, previous);
 	if (s.length() > 0) {
 		reverse(s);
-		s.add(s.length(), previous);
 	}
-	else {
-		s.add(s.length(), previous);
-	}
+	s.add(s.length(), previous);
 
 }//reverse
 
@@ -22,44 +19,41 @@ Integer findLargestValue(SequenceOfIntegers& s) {
 	Integer previous;
 	Integer next;
 	s.remove(0, previous);
-	if(s.length() > 0){
+	if (s.length() > 0) {	
 		next = findLargestValue(s);
 		Integer t;
 		t = previous;
 		s.add(0, t);
 		if (previous > next) return previous;
-		else return next;	
+		else return next;
 	}
-	else {
-		Integer temp = previous;
-		s.add(0, temp);
 		return previous;
-	}	
+	//}
 }//findLargestValue
 
-int main(){
-Integer t;
-SequenceOfIntegers seq1;
-t = 47;
-seq1.add(seq1.length(),t);
-t = -10;
-seq1.add(seq1.length(), t);
-t = -9;
-seq1.add(seq1.length(), t);
-t = 57;
-seq1.add(seq1.length(), t);
-t = 67;
-seq1.add(seq1.length(), t);
-t = -8;
-seq1.add(seq1.length(), t);
-t = 0;
-seq1.add(seq1.length(), t);
+int main() {
+	Integer t;
+	SequenceOfIntegers seq1;
+	t = 47;
+	seq1.add(seq1.length(), t);
+	t = -10;
+	seq1.add(seq1.length(), t);
+	t = -9;
+	seq1.add(seq1.length(), t);
+	t = 57;
+	seq1.add(seq1.length(), t);
+	t = 67;
+	seq1.add(seq1.length(), t);
+	t = -8;
+	seq1.add(seq1.length(), t);
+	t = 0;
+	seq1.add(seq1.length(), t);
 
-Integer z;
-z = findLargestValue(seq1);
-wcout << "The largest value in seq1 is: " << z << endl;
-wcout << "Sequence Before Reverse: " << seq1 << endl;
-reverse(seq1);
-wcout << "Sequence after Reverse: "<< seq1 << endl;
-return 0;
+	Integer z;
+	z = findLargestValue(seq1);
+	wcout << "The largest value in seq1 is: " << z << endl;
+	wcout << "Sequence Before Reverse: " << seq1 << endl;
+	reverse(seq1);
+	wcout << "Sequence after Reverse: " << seq1 << endl;
+	return 0;
 }
