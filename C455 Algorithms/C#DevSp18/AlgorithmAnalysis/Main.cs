006a2a30331eabac20d338477129509255821544
jb1361 <justinbutler4@hotmail.com>
2018-01-12 03:13:38 -0500

    C455 Project0Part 2 and Algorithm Analyzer

---
+++ C455 Algorithms/C#DevSp18/AlgorithmAnalysis/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmAnalysis
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+           ParseFile file = new ParseFile();
+           file.run();
+        }
+    }
+}
---
+++ C455 Algorithms/C#DevSp18/AlgorithmAnalysis/ParseFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace AlgorithmAnalysis
+{
+    class ParseFile
+    {
+        public void run()
+        {
+            string exePath = Environment.CurrentDirectory;
+            string filepath = exePath + "\\test.txt";
+            Console.WriteLine(filepath);
+            string[] text = System.IO.File.ReadAllLines(@filepath);
+            foreach (var item in text)
+            {
+                Console.WriteLine(item);
+            }
+            Analyze(text);
+        }
+        
+        public void Analyze(string[] algorithm)
+        {
+           
+        }
+       
+    
+      
+    }
+}
