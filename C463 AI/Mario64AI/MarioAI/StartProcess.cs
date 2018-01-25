aa42cc7157fa92c83beee48b7ce789eb2f6a64eb
jb1361 <justinbutler4@hotmail.com>
2018-01-25 06:01:58 -0500

    Project64 Source

--- C463 AI/Mario64AI/MarioAI/StartProcess.cs
+++ C463 AI/Mario64AI/MarioAI/StartProcess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,28 @@ namespace MarioAI
     {
         public void start()
         {
-            var proc = new Process
-            {
-                StartInfo = new ProcessStartInfo
+           // try
+           // {
+                Console.WriteLine("Starting Project64.");
+                var proc = new Process
                 {
-                    FileName = "program.exe",
-                    Arguments = "command line arguments to your executable",
-                    UseShellExecute = false,
-                    RedirectStandardOutput = true,
-                    CreateNoWindow = true
-                }
-            };
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        UseShellExecute = false,
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = false
+                    }
+                };
+                proc.Start();
+                proc.StandardInput.WriteLine("cd C:/Users/justi/Desktop/Class files repo/Class-files-repo/C463 AI/Mario64AI/Project64 2.3");
+                proc.StandardInput.WriteLine("Project64.exe"); 
+            //  }catch(Exception e)
+            //  {
+            //      Console.WriteLine("Couldn't start Project64, check that filepaths are correct.");
+            //  }
+            Console.WriteLine("Process Started.");
         }
     }
 }
