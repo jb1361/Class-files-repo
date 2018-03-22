555e4644ccd4a63a71ad513d0281d6f42f6b0f7a
Justin <justinbutler4@hotmail.com>
2018-03-22 17:56:06 -0400

    P4 Done

--- C455 Algorithms/P4/Program.cs
+++ C455 Algorithms/P4/Program.cs
@@ -5,24 +5,6 @@ using VertexMap = System.Collections.Generic.Dictionary<int, string>;
 
 namespace P4
 {
-    // Initial build and run
-    //   As the project stands just after downloading and unzipping, it will build and run
-    //   It will load GraphG1.txt and display its contents using GraphUtility operations
-    //   It works on an adjacency matrix graph representation
-
-    // To do:
-    //   Implement DFS_WordHunt and DfsVisit found in WordHunt.cs
-    //
-    // Testing:
-    // (1) Test with GraphG1.txt found in the P4 folder
-    //     The "words" output by your implementation do not have to be in the
-    //     same order as the "words" found in GraphG1-SolutionOutput.txt, but 
-    //     you do have to output all the same "words"
-    // (2) Create a .txt input file based on the graph found in WordHunt.jpg
-    //     Test with this data file
-    //     I'll be testing with wordLengthMax = 3 and = 5
-
-
     class MainClass
     {
         public static void Main(string[] args)
--- C455 Algorithms/P4/WordHunt.cs
+++ C455 Algorithms/P4/WordHunt.cs
@@ -12,43 +12,30 @@ namespace P4
 
         public void DfsVisit(int[,] graphRep, int u, int wordLengthMax, StringBuilder wordSoFar, VertexMap vertexLabels)
         {
-            
-            // With the regular DFS Visit a recursive call is made if the color of the vertex is white
-            // Here we have no colors, so what is the base case?
-
-            // We are at the base case when |wordSoFar| == wordLengthMax
-            // We are not at the base case when the |wordSoFar| < wordLengthMax
-
-            // When at base case
-            //   Output the wordSoFar using WriteLine and ToString on wordSoFar
-            //   The output word will have length = wordLengthMax
             if(wordSoFar.Length == wordLengthMax)
             {
                 string output = wordSoFar.ToString();
                 Console.WriteLine(output);
-            }
+            }          
             else if (wordSoFar.Length < wordLengthMax)
             {
-                foreach (var item in graphRep)
+                foreach (var item in vertexLabels)
                 {
-                    
+                    if(item.Key == u)
+                    {
+                        string x = item.Value;
+                        wordSoFar.Append(x);
+                    }                   
+                }              
+                for (int col = 0; col < graphRep.GetLength(0); col++)
+                {
+                    if (graphRep[u, col] == 1)
+                    {
+                        DfsVisit(graphRep, col, wordLengthMax, wordSoFar, vertexLabels);                       
+                    }
                 }
+                wordSoFar.Length--;
             }
-            // When not at base case
-            //   Use parameter u to obtain the character 'x' in graph that is associated with u
-            //   Add character 'x' to the end of wordSoFar
-            //   Then make a recursive call with each v that is adjacent to u
-            //   After the recursive call returns you must remove 'x' from the end of the wordSoFar
-
-            // To do:
-            //    Implement this operation based on Cormen's DFS-Visit and the instructions given above
-            //    There is no coloring, no keeping track of discovery time, or finish time, or predecessors
-
-            // Not allowed:
-            //    Changing the signature of this operation
-            //    Using globally declared variables
-            //    Using class-level declared variables
-
         } // DfsVisit
 
         public void DFS_WordHunt(int[,] graphRep, int wordLengthMax, VertexMap vertexLabels)
@@ -57,25 +44,10 @@ namespace P4
 
             int numberOfVertices = graphRep.GetLength(0);
 
-            // To do:
-            // Create for loop that calls DfsVisit with each vertex in the vertex set
-            // This will be very similar to Cormen's DFS, except DfsVisit must be called with all vertices
-            // And there is no coloring, no keeping track of discovery time, or finish time, or predecessors
             for (int row = 0; row < numberOfVertices; row++)
             {
-                for (int col = row; col < numberOfVertices; col++)
-                {
-                    if (graphRep[row, col] == 1)
-                    {
-                        
-                    }
-                }
+                DfsVisit(graphRep, row, wordLengthMax, wordSoFar, vertexLabels);
             }
-
-                        // Not allowed:
-                        //    Changing the signature of this operation
-                        //    Using globally declared variables
-                        //    Using class-level declared variables
         } // DFS_WordHunt
     }
 }
