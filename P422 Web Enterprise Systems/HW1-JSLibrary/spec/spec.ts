8e818dcce553159f05ae752dbb76fbddd1e35d9b
jb1361 <justinbutler4@hotmail.com>
2018-09-04 16:10:01 -0400

    Add tests for password functions

--- P422 Web Enterprise Systems/HW1-JSLibrary/spec/spec.ts
+++ P422 Web Enterprise Systems/HW1-JSLibrary/spec/spec.ts
@@ -11,4 +11,32 @@ describe('Password Checker', () => {
         const test = checker.lengthAboveEight('password');
         expect(test).toBe(false);
     });
+    it('password contains number pass', () => {
+        const test = checker.containsNumber('password1');
+        expect(test).toBe(true);
+    });
+    it('password contains number fail', () => {
+        const test = checker.containsNumber('password');
+        expect(test).toBe(false);
+    });
+    it('password contains uppercase letter pass', () => {
+        const test = checker.containsUppercase('Password');
+        expect(test).toBe(true);
+    });
+    it('password contains uppercase letter fail', () => {
+        const test = checker.containsUppercase('password');
+        expect(test).toBe(false);
+    });
+    it('password contains special character pass', () => {
+        const test = checker.containsSpecialCharacter('password!');
+        expect(test).toBe(true);
+    });
+    it('password contains special character fail', () => {
+        const test = checker.containsSpecialCharacter('password');
+        expect(test).toBe(false);
+    });
+    it('password rating above or equal to 8', () => {
+        const test =  checker.passwordRating('Testing1223???');
+        expect(test).toBeGreaterThanOrEqual(8);
+    });
 });
