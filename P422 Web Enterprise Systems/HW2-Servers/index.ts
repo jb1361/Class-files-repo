cbef67baffc5abb17c425e214f4b32f0baf7c432
jb1361 <justinbutler4@hotmail.com>
2018-09-17 21:01:00 -0400

    Fix express

--- P422 Web Enterprise Systems/HW2-Servers/index.ts
+++ P422 Web Enterprise Systems/HW2-Servers/index.ts
@@ -1,34 +1,20 @@
-import {Bloom} from './lib/Bloom';
-import {PswChecker} from './lib/pwchecker';
-const checker = new PswChecker();
+// import {Bloom} from './lib/Bloom';
+// import {PswChecker} from './lib/pwchecker';
+// const checker = new PswChecker();
 
-'use strict';
-const checkJwt = require('./middleware').checkJwt;
-//// Load dependencies
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const express = require('express');
-const router = require('./routes');
-const app = express();
-const cors = require('cors');
-const middleware = require('./middleware');
-// load environment
+// const checkJwt = require('./middleware').checkJwt;
+// //// Load dependencies
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+ import express = require('express');
+ const router = ('./routes');
+ const app = express();
+// const cors = require('cors');
+// const middleware = require('./middleware');
+// // load environment
 
-const config = require('./config');
+// const config = require('./config');
 
-if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
-    throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file';
-}
-
-// connect to mongodb
-mongoose.connect(`mongodb://${config.db.host}/${process.env.DB_NAME}`);
-
-
-// configure server
-app.use(cors({
-    origin: '*',
-    credentials:true
-}));
 
 app.use('/', express.static('client/src'));
 app.use(bodyParser.json());
