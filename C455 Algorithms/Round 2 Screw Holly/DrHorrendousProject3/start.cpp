a40050155daa9b1234fa4f9af002b602e639e2b8
jb1361 <justinbutler4@hotmail.com>
2019-04-21 00:12:33 -0400

    Update file reading

--- C455 Algorithms/Round 2 Screw Holly/DrHorrendousProject3/start.cpp
+++ C455 Algorithms/Round 2 Screw Holly/DrHorrendousProject3/start.cpp
@@ -3,11 +3,17 @@
 #include <cstring>
 
 using namespace std;
-
+class DirectedGraphMatrix {
+public:
+	int vertices[100][100] = {0};
+	void addVertice(int v1, int v2) {
+		vertices[v1][v2] = 1;
+	}
+};
 int main(int argc, char *argv[]) {
 	string input;
 	getline(cin, input);
-	
+	DirectedGraphMatrix* graph = new DirectedGraphMatrix();
 	while (input != "") {
 		char char_array[sizeof(input)];
 		strcpy_s(char_array, input.c_str());
@@ -15,8 +21,15 @@ int main(int argc, char *argv[]) {
 		pch = strtok(char_array, " ");
 		while (pch != NULL)
 		{
-			printf("%s\n", pch);
 			if (pch != "-1") {
+				int v1;				
+				if (v1 != NULL) {				
+					graph->addVertice(v1, stoi(pch));
+					v1 = NULL;					
+				}
+				else {
+					v1 = stoi(pch);
+				}				
 				pch = strtok(NULL, " ");
 			}
 			else {
@@ -26,5 +39,6 @@ int main(int argc, char *argv[]) {
 		}
 		getline(cin, input);
 	}
+	cout << graph->vertices << endl;
 	return 0;
 }
