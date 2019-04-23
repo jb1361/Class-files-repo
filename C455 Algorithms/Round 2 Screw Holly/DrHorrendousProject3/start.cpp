bf66936a41b18dbcf67bce54767cde630861943d
jb1361 <justinbutler4@hotmail.com>
2019-04-22 23:54:39 -0400

    Finish project 3

--- C455 Algorithms/Round 2 Screw Holly/DrHorrendousProject3/start.cpp
+++ C455 Algorithms/Round 2 Screw Holly/DrHorrendousProject3/start.cpp
@@ -1,44 +1,75 @@
 #include <iostream>
-#include <sstream>
-#include <cstring>
+#include <vector>
+#include <queue>
+#include <stack>
+
 
 using namespace std;
-class DirectedGraphMatrix {
-public:
-	int vertices[100][100] = {0};
-	void addVertice(int v1, int v2) {
-		vertices[v1][v2] = 1;
-	}
-};
-int main(int argc, char *argv[]) {
-	string input;
-	getline(cin, input);
-	DirectedGraphMatrix* graph = new DirectedGraphMatrix();
-	while (input != "") {
-		char char_array[sizeof(input)];
-		strcpy_s(char_array, input.c_str());
-		char* pch;
-		pch = strtok(char_array, " ");
-		while (pch != NULL)
-		{
-			if (pch != "-1") {
-				int v1;				
-				if (v1 != NULL) {				
-					graph->addVertice(v1, stoi(pch));
-					v1 = NULL;					
-				}
-				else {
-					v1 = stoi(pch);
-				}				
-				pch = strtok(NULL, " ");
-			}
-			else {
-				break;
-			}
 
+struct Vertex
+{
+	bool visited = false;
+	int parent = 0;
+	vector<int> neighbors = vector<int>();
+	int cost = 0;
+	int key = 0;
+};
+
+int main(int argc, char *argv[]) {
+	auto list = new vector<Vertex*>();
+
+	int vertex = 0;
+	cin >> vertex;
+	while (vertex != -1)
+	{
+		auto sublist = new Vertex();
+		sublist->key = vertex;
+		list->push_back(sublist);
+		cin >> vertex;
+		while (vertex != -1)
+		{
+			sublist->neighbors.push_back(vertex);
+			cin >> vertex;
 		}
-		getline(cin, input);
+		cin >> vertex;
 	}
-	cout << graph->vertices << endl;
+
+	auto q = new queue<Vertex*>();
+	q->push(list->front());
+	while (!q->empty()) {
+		auto v = q->front();
+		q->pop();
+		vector<int>::iterator i;
+		for (i = v->neighbors.begin(); i != v->neighbors.end(); i++)
+		{
+			Vertex* w = (*list)[*i - 1];
+			if (!w->visited) {
+				w->cost = v->cost + 1;
+				w->visited = true;
+				w->parent = v->key;
+				q->push(w);
+			}			
+		}		
+	}
+
+	stack<int> s = stack<int>();
+	Vertex* v = list->back();
+	cout << "Cost of shortest path: " << v->cost << endl;
+	while (v != NULL) {
+		s.push(v->key);
+		if (v->parent != 0) {
+			v = (*list)[v->parent - 1];
+		}
+		else v = NULL;
+	}
+	cout << "Path: {";
+	while (!s.empty()) {
+		cout << s.top();
+		s.pop();
+		if (!s.empty()) {
+			cout << ", ";
+		}
+	}
+	cout << "}";
 	return 0;
 }
